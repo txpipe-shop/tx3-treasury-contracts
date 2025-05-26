727c15db0b7f18c6eefedb4285fb0ab47990e685
Pi Lanningham <pi@sundaeswap.finance>
2025-05-25 22:38:43 -0400

    Add Test for MLabs 3.4

---
+++ offchain/tests/findings/mlabs.test.ts
@@ -0,0 +1,91 @@
+import { beforeEach, describe, test } from "bun:test";
+import { Core, makeValue } from "@blaze-cardano/sdk";
+import * as Data from "@blaze-cardano/data";
+import { Emulator } from "@blaze-cardano/emulator";
+import {
+  deployScripts,
+  findRegistryInput,
+  sampleTreasuryConfig,
+  sampleVendorConfig,
+  scriptOutput,
+  setupEmulator,
+} from "../utilities";
+import { loadScripts, unix_to_slot } from "../../src/shared";
+import { TreasurySpendRedeemer } from "../../src/types/contracts";
+
+describe("MLabs Audit Findings", () => {
+  let emulator: Emulator;
+  beforeEach(async () => {
+    emulator = await setupEmulator(undefined, false);
+  });
+
+  describe("3.4", () => {
+    test("can steal funds meant to be swept through double satisfaction", async () => {
+      const treasuryConfig1 = loadScripts(
+        Core.NetworkId.Testnet,
+        await sampleTreasuryConfig(emulator, 1),
+        await sampleVendorConfig(emulator, 1),
+      );
+      const treasuryConfig2 = loadScripts(
+        Core.NetworkId.Testnet,
+        await sampleTreasuryConfig(emulator, 2),
+        await sampleVendorConfig(emulator, 2),
+      );
+      const treasury1Input = scriptOutput(
+        emulator,
+        treasuryConfig1.treasuryScript,
+        makeValue(500_000_000_000n),
+        Data.Void(),
+      );
+
+      const treasury2Input = scriptOutput(
+        emulator,
+        treasuryConfig2.treasuryScript,
+        makeValue(500_000_000_000n),
+        Data.Void(),
+      );
+
+      await deployScripts(emulator, treasuryConfig1);
+      await deployScripts(emulator, treasuryConfig2);
+      const treasury1RefInput = emulator.lookupScript(
+        treasuryConfig1.treasuryScript.script.Script,
+      );
+      const treasury2RefInput = emulator.lookupScript(
+        treasuryConfig2.treasuryScript.script.Script,
+      );
+      const registryInput1 = findRegistryInput(emulator, 1);
+      const registryInput2 = findRegistryInput(emulator, 2);
+
+      emulator.stepForwardToUnix(
+        treasuryConfig1.treasuryScript.config.expiration + 1n,
+      );
+
+      await emulator.as("MaliciousUser", async (blaze) => {
+        await emulator.expectScriptFailure(
+          blaze
+            .newTransaction()
+            // Sweep
+            .addReferenceInput(treasury1RefInput)
+            .addReferenceInput(treasury2RefInput)
+            .addReferenceInput(registryInput1)
+            .addReferenceInput(registryInput2)
+            .addInput(
+              treasury1Input,
+              Data.serialize(TreasurySpendRedeemer, "SweepTreasury"),
+            )
+            .addInput(
+              treasury2Input,
+              Data.serialize(TreasurySpendRedeemer, "SweepTreasury"),
+            )
+            .setValidFrom(
+              unix_to_slot(
+                treasuryConfig1.treasuryScript.config.expiration + 1000n,
+              ),
+            )
+            .setDonation(500_000_000_000n),
+          /trace expect\s*inputs\s*|> list.all\(\s*fn\(input\) {\s*when input.output.address.payment_credential is {/,
+        );
+      });
+    });
+  });
+});
