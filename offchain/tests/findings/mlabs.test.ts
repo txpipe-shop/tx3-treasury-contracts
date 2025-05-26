182d10e4078fa4eea9e44fec1ad341ff05ef0588
Pi Lanningham <pi@sundaeswap.finance>
2025-05-26 12:42:26 -0400

    Fix MLabs 3.7 and 3.10

--- offchain/tests/findings/mlabs.test.ts
+++ offchain/tests/findings/mlabs.test.ts
@@ -1,20 +1,35 @@
 import { beforeEach, describe, test } from "bun:test";
-import { Core, makeValue } from "@blaze-cardano/sdk";
+import { Core, makeValue, Value } from "@blaze-cardano/sdk";
 import * as Data from "@blaze-cardano/data";
 import { Emulator } from "@blaze-cardano/emulator";
 import {
   deployScripts,
   findRegistryInput,
+  fund_key,
+  Funder,
   sampleTreasuryConfig,
   sampleVendorConfig,
   scriptOutput,
   setupEmulator,
+  vendor_key,
+  modify_key,
+  Modifier,
 } from "../utilities";
-import { loadScripts, unix_to_slot } from "../../src/shared";
 import {
+  coreValueToContractsValue,
+  loadScripts,
+  slot_to_unix,
+  unix_to_slot,
+} from "../../src/shared";
+import {
+  MultisigScript,
   TreasurySpendRedeemer,
+  VendorDatum,
   VendorSpendRedeemer,
 } from "../../src/types/contracts";
+import { fund } from "src/treasury";
+import { modify } from "src/vendor";
+import { Ed25519KeyHashHex } from "@cardano-sdk/crypto";
 
 describe("MLabs Audit Findings", () => {
   let emulator: Emulator;
@@ -23,7 +38,7 @@ describe("MLabs Audit Findings", () => {
   });
 
   describe("3.4", () => {
-    test("can steal funds meant to be swept through double satisfaction", async () => {
+    test("cannot steal funds meant to be swept through double satisfaction", async () => {
       const treasuryConfig1 = loadScripts(
         Core.NetworkId.Testnet,
         await sampleTreasuryConfig(emulator, 1),
@@ -93,7 +108,7 @@ describe("MLabs Audit Findings", () => {
   });
 
   describe("3.5", () => {
-    test("can steal funds meant to be swept through double satisfaction", async () => {
+    test("cannot steal funds meant to be swept through double satisfaction", async () => {
       const treasuryConfig = loadScripts(
         Core.NetworkId.Testnet,
         await sampleTreasuryConfig(emulator),
@@ -158,7 +173,7 @@ describe("MLabs Audit Findings", () => {
   });
 
   describe("3.6", () => {
-    test("can steal from the treasury reward account through double satisfaction", async () => {
+    test("cannot steal from the treasury reward account through double satisfaction", async () => {
       const config = loadScripts(
         Core.NetworkId.Testnet,
         await sampleTreasuryConfig(emulator),
@@ -211,6 +226,309 @@ describe("MLabs Audit Findings", () => {
               Data.Void(),
             ),
           /Trace expect None =\s*inputs\s*|> list.find\(\s*fn\(input\) {\s*or {\s*input.address.payment_credential == registry.treasury,/,
+        );
+      });
+    });
+  });
+
+  describe("3.7", () => {
+    async function mockVendorDatum(
+      emulator: Emulator,
+      vendorCount: number,
+      payoutCount: number,
+      valueSize: number,
+    ): Promise<
+      [
+        MultisigScript,
+        VendorDatum,
+        Array<{ date: Date; amount: Core.Value }>,
+        Core.Value,
+      ]
+    > {
+      const vendor: MultisigScript = {
+        AllOf: {
+          scripts: [],
+        },
+      };
+      const vendorKey = await vendor_key(emulator);
+      for (let i = 0; i < vendorCount; i++) {
+        vendor.AllOf.scripts.push({ Signature: { key_hash: vendorKey } });
+      }
+      const datum: VendorDatum = {
+        vendor,
+        payouts: [],
+      };
+      const schedule = Array<{ date: Date; amount: Core.Value }>();
+
+      let totalValue = Value.zero();
+      for (let i = 0; i < payoutCount; i++) {
+        // set all payout's maturation to 1h before treasury expiratoin
+        const assets: [string, bigint][] = [];
+        for (let j = 0; j < valueSize; j++) {
+          const policyId = `${"0".repeat(50)}${Number(j).toString().padStart(6, "0")}`;
+          assets.push([policyId, 1_000_000n]);
+        }
+        const value = makeValue(1_000_000n, ...assets);
+        totalValue = Value.merge(totalValue, value);
+        const translatedValue = coreValueToContractsValue(value);
+
+        datum.payouts.push({
+          maturation: 0n,
+          value: translatedValue,
+          status: "Active",
+        });
+
+        schedule.push({
+          date: new Date(Number(slot_to_unix(Core.Slot(0)))),
+          amount: value,
+        });
+      }
+      return [vendor, datum, schedule, totalValue];
+    }
+
+    // Benchmarking shows that we hit execution unit limits at around:
+    // - 35 payouts with 3 tokens (+ ada) in each value;
+    // - 24 payouts with 5 tokens (+ ada) in each value;
+    // So setting 24 payouts (twice a month for a year) with at most 3 tokens (USDM + USDA + one other) (plus ADA)
+    // seems gives us some wiggle room.
+    // If we need more than that, we can always split across multiple projects
+    test("cannot permanently deadlock funds in projects with more than 24 payouts", async () => {
+      const configs = loadScripts(
+        Core.NetworkId.Testnet,
+        await sampleTreasuryConfig(emulator),
+        await sampleVendorConfig(emulator),
+      );
+      await deployScripts(emulator, configs);
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const [vendor, _, schedule, value] = await mockVendorDatum(
+        emulator,
+        1,
+        25,
+        3,
+      );
+      const treasuryInput = scriptOutput(
+        emulator,
+        configs.treasuryScript,
+        value,
+        Data.Void(),
+      );
+      await emulator.as(Funder, async (blaze) => {
+        const tx = await fund(
+          {
+            treasury: configs.treasuryScript.config,
+            vendor: configs.vendorScript.config,
+          },
+          blaze,
+          treasuryInput,
+          vendor,
+          schedule,
+          [Core.Ed25519KeyHashHex(await fund_key(emulator))],
+        );
+        emulator.expectScriptFailure(tx, /Trace expect payout_count <= 24/);
+      });
+    });
+    test("cannot permanently deadlock funds in projects with more than 3 tokens", async () => {
+      const configs = loadScripts(
+        Core.NetworkId.Testnet,
+        await sampleTreasuryConfig(emulator),
+        await sampleVendorConfig(emulator),
+      );
+      await deployScripts(emulator, configs);
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const [vendor, _, schedule, value] = await mockVendorDatum(
+        emulator,
+        1,
+        24,
+        4,
+      );
+      const treasuryInput = scriptOutput(
+        emulator,
+        configs.treasuryScript,
+        value,
+        Data.Void(),
+      );
+      await emulator.as(Funder, async (blaze) => {
+        const tx = await fund(
+          {
+            treasury: configs.treasuryScript.config,
+            vendor: configs.vendorScript.config,
+          },
+          blaze,
+          treasuryInput,
+          vendor,
+          schedule,
+          [Core.Ed25519KeyHashHex(await fund_key(emulator))],
+        );
+        emulator.expectScriptFailure(
+          tx,
+          /Trace expect 4 >= \( value |> assets.flatten |> list.length \)/,
+        );
+      });
+    });
+    test("cannot permanently deadlock funds in modified projects with more than 24 payouts", async () => {
+      const configs = loadScripts(
+        Core.NetworkId.Testnet,
+        await sampleTreasuryConfig(emulator),
+        await sampleVendorConfig(emulator),
+      );
+      await deployScripts(emulator, configs);
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const [_, smallVendorDatum, __] = await mockVendorDatum(
+        emulator,
+        1,
+        1,
+        1,
+      );
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const [___, largeVendorDatum, ____, largeValue] = await mockVendorDatum(
+        emulator,
+        1,
+        25,
+        1,
+      );
+      const vendorInput = scriptOutput(
+        emulator,
+        configs.vendorScript,
+        largeValue,
+        Data.serialize(VendorDatum, smallVendorDatum),
+      );
+
+      await emulator.as(Modifier, async (blaze) => {
+        const tx = await modify(
+          {
+            treasury: configs.treasuryScript.config,
+            vendor: configs.vendorScript.config,
+          },
+          blaze,
+          new Date(Number(slot_to_unix(Core.Slot(0)))),
+          vendorInput,
+          largeVendorDatum,
+          [
+            Core.Ed25519KeyHashHex(await modify_key(emulator)),
+            Core.Ed25519KeyHashHex(await vendor_key(emulator)),
+          ],
+        );
+        emulator.expectScriptFailure(tx, /Trace expect payout_count <= 24/);
+      });
+    });
+    test("cannot permanently deadlock funds in modified projects with more than 3 tokens", async () => {
+      const configs = loadScripts(
+        Core.NetworkId.Testnet,
+        await sampleTreasuryConfig(emulator),
+        await sampleVendorConfig(emulator),
+      );
+      await deployScripts(emulator, configs);
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const [_, smallVendorDatum, __] = await mockVendorDatum(
+        emulator,
+        1,
+        1,
+        1,
+      );
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const [___, largeVendorDatum, ____, largeValue] = await mockVendorDatum(
+        emulator,
+        1,
+        1,
+        4,
+      );
+      const vendorInput = scriptOutput(
+        emulator,
+        configs.vendorScript,
+        largeValue,
+        Data.serialize(VendorDatum, smallVendorDatum),
+      );
+
+      await emulator.as(Modifier, async (blaze) => {
+        const tx = await modify(
+          {
+            treasury: configs.treasuryScript.config,
+            vendor: configs.vendorScript.config,
+          },
+          blaze,
+          new Date(Number(slot_to_unix(Core.Slot(0)))),
+          vendorInput,
+          largeVendorDatum,
+          [
+            Core.Ed25519KeyHashHex(await modify_key(emulator)),
+            Core.Ed25519KeyHashHex(await vendor_key(emulator)),
+          ],
+        );
+        emulator.expectScriptFailure(
+          tx,
+          /Trace expect 4 >= \( value |> assets.flatten |> list.length \)/,
+        );
+      });
+    });
+  });
+
+  describe("3.10", () => {
+    test("cannot create underfunded project during modification", async () => {
+      const config = loadScripts(
+        Core.NetworkId.Testnet,
+        await sampleTreasuryConfig(emulator),
+        await sampleVendorConfig(emulator),
+      );
+      await deployScripts(emulator, config);
+
+      const vendor = {
+        Signature: {
+          key_hash: await vendor_key(emulator),
+        },
+      };
+      const modifySigner = Ed25519KeyHashHex(await modify_key(emulator));
+      const vendorSigner = Ed25519KeyHashHex(await vendor_key(emulator));
+      const vendorDatum: VendorDatum = {
+        vendor: vendor,
+        payouts: [
+          {
+            maturation: 0n,
+            status: "Active",
+            value: coreValueToContractsValue(makeValue(500_000_000_000n)),
+          },
+        ],
+      };
+      const vendorInput = scriptOutput(
+        emulator,
+        config.vendorScript,
+        makeValue(500_000_000_000n),
+        Data.serialize(VendorDatum, vendorDatum),
+      );
+
+      const vendorRefInput = emulator.lookupScript(
+        config.vendorScript.script.Script,
+      );
+      const registryInput = findRegistryInput(emulator);
+      const invalidDatum: VendorDatum = {
+        vendor: vendor,
+        payouts: [
+          {
+            maturation: 0n,
+            status: "Active",
+            value: coreValueToContractsValue(makeValue(500_000_000_100n)),
+          },
+        ],
+      };
+      await emulator.as(Modifier, async (blaze) => {
+        emulator.expectScriptFailure(
+          blaze
+            .newTransaction()
+            .addReferenceInput(registryInput)
+            .addReferenceInput(vendorRefInput)
+            .setValidFrom(Core.Slot(0))
+            .setValidUntil(Core.Slot(100))
+            .addInput(
+              vendorInput,
+              Data.serialize(VendorSpendRedeemer, "Modify"),
+            )
+            .lockAssets(
+              config.vendorScript.scriptAddress,
+              makeValue(500_000_000_000n),
+              Data.serialize(VendorDatum, invalidDatum),
+            )
+            .addRequiredSigner(modifySigner)
+            .addRequiredSigner(vendorSigner),
+          /Trace expect equal_plus_min_ada\(this_payout_sum, output.value\)/,
         );
       });
     });
