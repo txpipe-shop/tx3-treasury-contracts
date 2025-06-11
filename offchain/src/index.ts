034671012a15731fd0c04ef916f490ea69c9decd
Calvin Koepke <hello@calvinkoepke.com>
2025-06-11 15:35:11 -0600

    chore: expose metadata funcs

--- offchain/src/index.ts
+++ offchain/src/index.ts
@@ -1,3 +1,4 @@
 export * as Contracts from "./generated-types/contracts.js";
+export * as Metadata from "./metadata/shared.js";
 export * as Treasury from "./treasury/index.js";
 export * as Vendor from "./vendor/index.js";
