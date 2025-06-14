55c3e93a4c6477fe54fb324c1c23d2c1c67b3bbc
Calvin Koepke <hello@calvinkoepke.com>
2025-06-13 23:39:53 -0600

    fix: export

--- offchain/src/index.ts
+++ offchain/src/index.ts
@@ -1,12 +1,6 @@
+export * from "./generated-types/contracts.js";
 export * as Contracts from "./generated-types/contracts.js";
 export * as Metadata from "./metadata/shared.js";
+export * from "./metadata/types/index.js";
 export * as Treasury from "./treasury/index.js";
 export * as Vendor from "./vendor/index.js";
-
-import * as GeneratedTypes from "./generated-types/contracts.js";
-import * as InternalTypes from "./metadata/types/index.js";
-
-export const Types = {
-  ...InternalTypes,
-  ...GeneratedTypes,
-};
