dfbc9d4010d5b34e561c4c3c8134bab9b7e3ef4d
Calvin Koepke <hello@calvinkoepke.com>
2025-06-14 20:10:54 -0600

    chore: export utils

--- offchain/src/index.ts
+++ offchain/src/index.ts
@@ -2,5 +2,6 @@ export * from "./generated-types/contracts.js";
 export * as Contracts from "./generated-types/contracts.js";
 export * as Metadata from "./metadata/shared.js";
 export * from "./metadata/types/index.js";
+export * as Utils from "./shared/index.js";
 export * as Treasury from "./treasury/index.js";
 export * as Vendor from "./vendor/index.js";
